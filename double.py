532dc60ce9edc622d4ee36e5ecdf3d30a362771a
mustafa can nacak <54077322+cannacak@users.noreply.github.com>
2023-01-24 16:49:14 +0300

    Add files via upload

---
+++ double.py
@@ -0,0 +1,12 @@
+import numpy as np
+def double(in) -> 'double type array':
+
+    out = in.astype(np.float32)
+
+    out /= 255 #to be able to divide by 255 it has to be converted float32 datatype
+    out = np.ceil(out) #all the values except '0's become '1'. Same as double() in MatLab
+
+    out = out.astype(np.uint8) 
+    out  *= 255 #rescaling
+
+    return out
